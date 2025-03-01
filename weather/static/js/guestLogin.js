ed0ffa1a669be52405267f35cf30641e7b548c95
Skye <swskye17@gmail.com>
2025-03-01 22:49:38 +0300

    Add guest loging loading display
    Signed-off-by: Skye <swskye17@gmail.com>

--- weather/static/js/guestLogin.js
+++ weather/static/js/guestLogin.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (guestLoginBtn) {
         guestLoginBtn.addEventListener("click", function (event) {
             event.stopPropagation();
-             window.HandleLoading('show', 'Just a moment. Logging in ...')
+            HandleLoading('show', 'Just a moment. Logging in ...')
             fetch("/api/guest-login/", {
                 method: "POST",
                 headers: {
@@ -16,11 +16,56 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (response.status === 200) {
                     window.location.href = "/home/";
                 } else {
-                     window.HandleLoading('hide')
-                      window.displayStatus('Error', `Failed to login: ${error}`)
+                    HandleLoading('hide')
+                    displayStatus('Error', `Failed to login: ${error}`)
                     alert("Guest login failed. Please try again.");
                 }
             });
         });
+    }
+
+
+    function HandleLoading(task, message=null){
+
+        const loadingModal = document.getElementById('loadingModal');
+        const modalMainBox = document.getElementById('modalMainBox');
+        if (task==="show"){
+            if (message){
+                const msgBox = document.getElementById('loadingMSG');
+                msgBox.textContent = message;
+            }
+            loadingModal.classList.remove('hidden');
+            modalMainBox.classList.remove('animate-exit');
+            modalMainBox.classList.add('animate-enter')
+        }else{
+            modalMainBox.classList.remove('animate-enter')
+            modalMainBox.classList.add('animate-exit');
+            setTimeout( ()=> {
+                loadingModal.classList.add('hidden');
+            }, 310)
+        }
+    }
+
+    function displayStatus(message=null, type='success'){
+        const modal = type==='success' ? document.getElementById('successModal') : document.getElementById('errorModal');
+        const box = type==='success' ? document.getElementById('successBox') : document.getElementById('errorBox');
+        if(message){
+            const mesSection = type==='success' ? document.getElementById('success-message') : document.getElementById('error-message');
+            mesSection.textContent = message;
+        }
+        modal.classList.remove('hidden');
+        box.classList.remove('animate-exit');
+        box.classList.add('animate-enter')
+    }
+
+    function hideStatus(type){
+        const modal = type==='success' ? document.getElementById('successModal') : document.getElementById('errorModal');
+        const box = type==='success' ? document.getElementById('successBox') : document.getElementById('errorBox');
+
+        box.classList.remove('animate-enter')
+        box.classList.add('animate-exit');
+        setTimeout( ()=> {
+            modal.classList.add('hidden');
+        }, 310)
     }
 });
